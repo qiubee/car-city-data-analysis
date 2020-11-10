04322db9b194acf277fe716518999aaed47eb11c
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-10 17:26:54 +0100

    add packages & function to get gpkg layer names

--- geometry.py
+++ geometry.py
@@ -1,4 +1,27 @@
 import geopandas as gp
+import pandas as pd
+import fiona
 
-GEBIED_IND = gp.read_file("datasets/CBS/cbsgebiedsindelingen_2021_v1.gpkg")
-print(GEBIED_IND)
+GPKG_FILE = "datasets/CBS/cbsgebiedsindelingen_2021_v1.gpkg"
+
+
+def get_gpkg_layer_names(geopkg_src):
+    layers = []
+    for layername in fiona.listlayers(geopkg_src):
+        layers.append(layername)
+    return layers
+
+
+layers = get_gpkg_layer_names(GPKG_FILE)
+# print(layers)
+
+landsdeel_2021_id = layers.index("cbs_landsdeel_2021_gegeneraliseerd")
+gemeente_2021_id = layers.index("cbs_gemeente_2021_labelpoint")
+gemeente_2020_id = layers.index("cbs_gemeente_2020_gegeneraliseerd")
+
+GEMEENTE_2021_IND = layers[gemeente_2021_id]
+LANDSDEEL_2021 = layers[landsdeel_2021_id]
+GEMEENTE_2020_IND = layers[gemeente_2020_id]
+
+data = gp.read_file(GPKG_FILE, layer=GEMEENTE_2021_IND)
+print(data)
