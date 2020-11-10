f1a44ca77132186877fbbe742f79b26e9e183061
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-10 23:04:47 +0100

    update column names & column drops

--- tabular_text.py
+++ tabular_text.py
@@ -11,11 +11,13 @@ def write_csv(data, filename):
     print(file, "is created in folder: data")
 
 
-nl_postal_columns = ["CountryCode", "PostalCode", "Place", "Province", "ProvinceId", "f", "g", "h", "i", "Lat", "Long", "l"]
+geo_postal_columns = ["countryCode", "postalCode", "placeName", "province", "adminCode1", "municipality", "adminCode2", "community", "adminCode3", "latitude", "longitude", "accuracy"]
 nl_postal = pd.read_table("datasets/geonames/NL_full/NL_full.txt", header=None)
-nl_postal.columns = nl_postal_columns
-nl_postal = nl_postal.drop(nl_postal.iloc[:, 4:12], axis=1)
+nl_postal.columns = geo_postal_columns
+nl_postal = nl_postal.drop(nl_postal.columns[[4, 6, 7, 8, 11]], axis=1)
 nl_postal = nl_postal.drop(nl_postal.iloc[:, :2], axis=1)
-nl_postal = nl_postal.drop_duplicates()
 
-write_csv(nl_postal, "nl_postal")
+# nl_postal = nl_postal.drop_duplicates()
+print(nl_postal)
+
+# write_csv(nl_postal, "nl_postal")
