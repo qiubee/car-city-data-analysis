5bda2d62a69ef2cba982692737042f886a2693d1
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-10 21:34:17 +0100

    remove unused columns

--- tabular_text.py
+++ tabular_text.py
@@ -14,6 +14,8 @@ def write_csv(data, filename):
 nl_postal_columns = ["CountryCode", "PostalCode", "Place", "Province", "ProvinceId", "f", "g", "h", "i", "Lat", "Long", "l"]
 nl_postal = pd.read_table("datasets/geonames/NL_full/NL_full.txt", header=None)
 nl_postal.columns = nl_postal_columns
-nl_postal = nl_postal.drop(nl_postal.columns[[0, 4, 5, 6, 7, 8, 11]], axis=1)
-nl_postal.index.name = "Id"
+nl_postal = nl_postal.drop(nl_postal.iloc[:, 4:12], axis=1)
+nl_postal = nl_postal.drop(nl_postal.iloc[:, :2], axis=1)
+nl_postal = nl_postal.drop_duplicates()
+
 write_csv(nl_postal, "nl_postal")
