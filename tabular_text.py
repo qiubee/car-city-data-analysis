d6fa56ec9f714743502fb515ec2dd4051ba090df
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-10 16:09:38 +0100

    remove general data and add postal data

--- tabular_text.py
+++ tabular_text.py
@@ -11,8 +11,8 @@ def write_csv(data, filename):
     print(file, "is created in folder: data")
 
 
-columns = ["ItemNumber", "PlaceName", "AreaName", "Namings", "Lat", "Long", "g", "h", "CountryCode", "j", "k", "l", "m", "n", "o", "p", "q", "Capital", "DateOfInput"]
-geo_names = pd.read_table("datasets/geonames/NL/NL.txt", header=None)
-geo_names.columns = columns
-print(geo_names)
-write_csv(geo_names, "nl")
+nl_postal_columns = ["CountryCode", "PostalCode", "Place", "Province", "ProvinceId", "f", "g", "h", "i", "Lat", "Long", "l"]
+nl_postal = pd.read_table("datasets/geonames/NL_full/NL_full.txt", header=None)
+nl_postal.columns = nl_postal_columns
+nl_postal = nl_postal.drop(nl_postal.columns[[0, 4, 5, 6, 7, 8, 11]], axis=1)
+write_csv(nl_postal, "nl_postal")
