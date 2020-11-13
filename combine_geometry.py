707460b1ba7d4599a983ee523154437298e0ee38
qiubee <45637654+qiubee@users.noreply.github.com>
2020-11-13 18:24:31 +0100

    fix calculate sum, merge with geodata & export csv

--- combine_geometry.py
+++ combine_geometry.py
@@ -1,12 +1,10 @@
+from pathlib import Path
+from os import chdir
 import pandas as pd
-from operator import itemgetter
-from itertools import groupby
-from collections import defaultdict
-from geojson import MultiPolygon
-
 
 NL_POSTAL = pd.read_csv("data/nl_postal.csv")
 NL_PROVINCE_GEO = pd.read_csv("data/cbs_provincie_2020_gegeneraliseerd.csv")
+NL_MUNICIPALITY_GEO = pd.read_csv("data/cbs_gemeente_2020_gegeneraliseerd.csv")
 RDW_DATASET = pd.read_csv("data/RDW_dataset.csv")
 
 
@@ -14,33 +12,52 @@ def drop_df_columns(df, columns):
     return df.drop(df.columns[columns], axis=1)
 
 
+def write_csv(data, filename):
+    Path("data").mkdir(parents=True, exist_ok=True)
+    chdir("data")
+    file = f"{filename}.csv"
+    data.to_csv(file)
+    print(file, "is created in folder: data")
+    chdir("../")
+
+
+def group_and_sum(df, group_columns, to_sum_columns):
+    return df.groupby(group_columns, as_index=False)[to_sum_columns].sum()
+
+
 # drop unused columns
 drop_rdw_columns = [0, 2, 3, 4, 6, 9]
 rdw = drop_df_columns(RDW_DATASET, drop_rdw_columns)
 
-# match merge column & merge
+# match column & merge
 rename_nl_col = {"placeName": "AreaManagerDesc"}
 nl_place_hierarchy = drop_df_columns(NL_POSTAL, [3, 4])
 nl_place_hierarchy = nl_place_hierarchy.rename(columns=rename_nl_col)
 rdw_geo = pd.merge(rdw, nl_place_hierarchy, how="left", on=["AreaManagerDesc"])
 
 # add sum of usage type in new column
-rdw_geo["UsageType_count"] = rdw_geo.groupby("UsageType_Id")["UsageType_Id"].transform(lambda x: x.count())
+rdw_geo["UsageType_count"] = rdw_geo.groupby(["AreaManagerDesc", "UsageType_Id"])["UsageType_Id"].transform(lambda x: x.count())
 
-# count sum of boolean columns
-province_cols = [rdw_geo.columns.values[i] for i in [4, 5, 7]]
-province_geo = rdw_geo.groupby(province_cols, as_index=False)[["ExitPossibleAllDay", "OpenAllYear"]].sum()
+# count sum of boolean columns for municipalities
+mp_sum_cols = ["ExitPossibleAllDay", "OpenAllYear"]
+mp_group_cols = [rdw_geo.columns.values[i] for i in [4, 5, 6, 7]]
+mp_summed = group_and_sum(rdw_geo, mp_group_cols, mp_sum_cols)
 
-# convert DataFrame to dictionary
-province_dict = province_geo.to_dict("records")
+# count sum of municipalities
+mp_data = mp_summed.groupby(["municipality", "UsageType_Id", "province"], as_index=False).sum()
 
-# group by province
-province_dict = sorted(province_dict, key=itemgetter("province"))
-res = defaultdict(list)
-for key, group in groupby(province_dict, key=itemgetter("province")):
-    res[key] = key
-    # res["province"] = key
-    for val in group:
-        print(val)
+# count sum of  province
+pv_sum_col = ["ExitPossibleAllDay", "OpenAllYear", "UsageType_count"]
+pv_group_cols = [rdw_geo.columns.values[i] for i in [4, 5]]
+pv_data = group_and_sum(mp_data, pv_group_cols, pv_sum_col)
 
-print(res)
+# add geometry to province & municipality
+rename_to_pv = {"statnaam": "province"}
+rename_to_mp = {"statnaam": "municipality"}
+pv_geo = NL_PROVINCE_GEO.rename(columns=rename_to_pv)
+mp_geo = NL_MUNICIPALITY_GEO.rename(columns=rename_to_mp)
+pv_complete = pd.merge(pv_geo, rdw_geo, how="left", on=["province"])
+mp_complete = pd.merge(mp_geo, rdw_geo, how="left", on=["municipality"])
+
+write_csv(pv_complete, "province")
+write_csv(mp_complete, "municipality")
